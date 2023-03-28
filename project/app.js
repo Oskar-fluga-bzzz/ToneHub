3107357b251b2460e2815dab68a2d6ab0ae0af64
Fluga bzzz <116791678+Oskar-fluga-bzzz@users.noreply.github.com>
2023-03-28 09:48:56 +0000

    0.1

--- project/app.js
+++ project/app.js
@@ -1,7 +1,26 @@
-let accessToken = 'BQA7nEt32CdGDUPxl-nyXp2yx4sbR-RXcL9MDKkOJBnNCqLgrNtBZ2dXhfYeMzKUI8NGLs0eK5305UDppPV9ucgxn92jNn1QUwgWEVOICZWkrUHiDqCMG-QOt16AKeyX4KwZCnIp8nR2FqH1WQNrXBe_vhHuuxvThuDfbWzUpgs1GAkj9u65di9IUrpfFvnuwTrdT7hQ'
+import {useEffect } from 'react'
 
 
-  
+// API Access Token
+const CLIENT_ID = '7269f024721e4773b4d4251e0616a20e'
+const CLIENT_SECRET = '51bd5effc3aa4aea9a880d677034eb09'
+
+useEffect(() => {
+    var authParameters = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-from-urlencoded'
+        },
+        body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+    }
+
+    fetch('https://accounts.spotify.com/api/token', authParameters)
+        .then(result => result.json)
+        .then(data => console.log(data))
+}, [])
+
+
+  // use artist name to get top tracks
     let artistName = "radiohead"
     let countryCode = 'GB'
     fetch(`https://api.spotify.com/v1/search?type=artist&q=${artistName}`, {
@@ -21,4 +40,3 @@ let accessToken = 'BQA7nEt32CdGDUPxl-nyXp2yx4sbR-RXcL9MDKkOJBnNCqLgrNtBZ2dXhfYeM
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error(error));
-
