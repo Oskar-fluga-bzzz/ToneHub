ec986afc17cbcb42b0f40f237d4fc086a7eb7e0b
Oskar-fluga-bzzz <oskar.hughes@skola.taby.se>
2023-03-29 12:16:10 +0200

    0.3

--- project/app.js
+++ project/app.js
@@ -1,10 +1,8 @@
 
-    // --- Hämtar en Spotify API Access Token --- //
-
+// --- Hämtar en Spotify API Access Token --- //
 const CLIENT_ID = '7269f024721e4773b4d4251e0616a20e'
 const CLIENT_SECRET = '51bd5effc3aa4aea9a880d677034eb09'
 let accessToken = ''
-
 function tokenRequest() {
     var authParameters = {
         method: 'POST',
@@ -13,7 +11,6 @@ function tokenRequest() {
         },
         body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
     }
-
     return fetch('https://accounts.spotify.com/api/token', authParameters)
         .then(res => res.json())
         .then(data => {
@@ -25,23 +22,32 @@ function tokenRequest() {
             accessToken = token})
         .catch(error => console.error(error))
 }
-window.onload = tokenRequest()
 
 
+// --- Kör funktionen --- //
+tokenRequest() .then(accessToken => {
 
-// --- söker efter en artist och returnerar spotify id for artisten som är högst upp i sökresultaten --- //
 
-let searchQuery = "radiohead"
+// --- Sökparametrar --- //
+let searchString = "billie eilish"
 let searchType = "artist"
 let artistID = ""
-let countryCode = "GB"
-fetch(`https://api.spotify.com/v1/search?type=${searchType}&q=${searchQuery}`, {
+let countryCode = "SE"
+let resultLimit = ""
+let resultOffset = ""
+let includeExternalAudio = ""
+
+
+// --- söker efter en artist och returnerar spotify id for artisten som är högst upp i resultaten --- //
+fetch(`https://api.spotify.com/v1/search?type=${searchType}&q=${searchString}`, {
     headers: {
         'Authorization': 'Bearer ' + accessToken
     }
 })
 .then(response => response.json())
 .then(data =>
-console.log('artist id: ' + data.artists.items[0].id,
-artistID = data.artists.items[0].id))
+console.log(data.artists.items[0].name + ', ' + data.artists.items[0].id))
 .catch(error => console.error(error))
+
+
+});
